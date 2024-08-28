de7f098e6f0e37e7e4cf4b4bd6addc7a1643aca0
Gabriela Arroyo <gabriela.hispanicas@gmail.com>
2024-08-27 19:50:53 -0600

    Adición de la funcionalidad para desencriptar un texto

--- app.js
+++ app.js
@@ -37,3 +37,40 @@ function encriptar() {
 
         
 }
+
+function desencriptar() {
+
+    let texto = document.getElementById('texto').value;
+
+    let textoCifrado = texto
+        .replace(/enter/gi, 'e')
+        .replace(/imes/gi, 'i')
+        .replace(/ai/gi, 'a')
+        .replace(/ober/gi, 'o')
+        .replace(/ufat/gi, 'u')
+
+        if (texto.length != 0){
+
+            document.getElementById('texto').value = textoCifrado;
+
+            tituloMensaje.textContent = 'Texto desencriptado con éxito';
+    
+            parrafo.textContent = '';
+    
+            muñeco.src = './img/encriptado.jpg';
+
+        } else {
+
+            muñeco.src = './img/muñeco.png';
+
+            tituloMensaje.textContent = 'Ningún mensaje fue encontrado';
+
+            parrafo.textContent = 'Ingresa el texto que deseas encriptar o desencriptar';
+
+            alert('Debes ingresar algún texto');
+
+
+
+        }
+
+} 
